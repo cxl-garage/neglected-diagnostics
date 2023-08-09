dfb041d8678f5fc016eac67a90a83e96354b46e8
Aniket Fadia <aniketf@uw.edu>
2023-08-09 13:53:39 -0700

    Correct Type Hints for filters_info.py

--- src/app/frontend/filters_info.py
+++ src/app/frontend/filters_info.py
@@ -1,15 +1,17 @@
+from typing import Any, Dict
+
 import streamlit as st
 
 EMPTY_FILTERS = "No filters applied"
 NON_EMPTY_FILTERS = "Filters applied -\n\n"
 
 
-def show_filters_info(applied_filters: dict) -> None:
+def show_filters_info(applied_filters: Dict[str, Any]) -> None:
     """Display information about applied filters.
 
     Parameters
     ----------
-    applied_filters : dict
+    applied_filters : Dict[str, Any]
         A dictionary containing the applied filters, where keys are the filter descriptions and values represent the corresponding filter values.
 
     Returns
@@ -17,7 +19,7 @@ def show_filters_info(applied_filters: dict) -> None:
     None
     """
     filters_info = EMPTY_FILTERS
-    
+
     # Filters applied
     if applied_filters:
         applied_filters_str = "\n\n".join(
