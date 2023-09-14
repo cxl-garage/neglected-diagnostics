26c7e9bd75288ea0d31925fc5182ae85fe3b65fb
Aniket Fadia <aniketf@uw.edu>
2023-09-14 11:48:32 -0700

    Update test input format to dict of tuple

--- tests/genetic_testing/sequence_analysis/test_sequence_variability.py
+++ tests/genetic_testing/sequence_analysis/test_sequence_variability.py
@@ -15,62 +15,45 @@ EXPECTED_RESULT = "expected_result"
 
 # Define expected results
 EXPECTED_RESULTS = {
-    "empty_sequences": {
-        SEQUENCES: {},
-        BASE_SEQUENCE: "ACGT",
-        MIN_COUNT: 2,
-        EXPECTED_RESULT: None,
-    },
-    "empty_base_sequence": {
-        SEQUENCES: {"seq1": "ACGT"},
-        BASE_SEQUENCE: "",
-        MIN_COUNT: 2,
-        EXPECTED_RESULT: None,
-    },
-    "single_sequence": {
-        SEQUENCES: {"seq1": "ACGT"},
-        BASE_SEQUENCE: "ACGT",
-        MIN_COUNT: 1,
-        EXPECTED_RESULT: pd.DataFrame(
+    "empty_sequences": ({}, "ACGT", 2, None),
+    "empty_base_sequence": ({"seq1": "ACGT"}, "", 2, None),
+    "single_sequence": (
+        {"seq1": "ACGT"},
+        "ACGT",
+        1,
+        pd.DataFrame(
             {"Sequence": ["ACGT"], "Count": [1], "Group_Number": [1]}
         ),  # Test case for single sequence
-    },
-    "multiple_sequence_same_group": {
-        SEQUENCES: {"seq1": "-CGT", "seq2": "ACGT"},
-        BASE_SEQUENCE: "ACGT",
-        MIN_COUNT: 1,
-        EXPECTED_RESULT: pd.DataFrame(
+    ),
+    "multiple_sequence_same_group": (
+        {"seq1": "-CGT", "seq2": "ACGT"},
+        "ACGT",
+        1,
+        pd.DataFrame(
             {"Sequence": ["ACGT"], "Count": [2], "Group_Number": [1]}
         ),  # Test case for multiple sequences belonging to same group
-    },
-    "multiple_sequence_different_group": {
-        SEQUENCES: {
+    ),
+    "multiple_sequence_different_group": (
+        {
             "seq1": "-CGT",
             "seq2": "ACGT",
             "seq3": "TGCA",
             "seq4": "TGCA",
             "seq5": "TGC-",
         },
-        BASE_SEQUENCE: "ACGT",
-        MIN_COUNT: 2,
-        EXPECTED_RESULT: pd.DataFrame(
+        "ACGT",
+        2,
+        pd.DataFrame(
             {"Sequence": ["ACGT", "TGCA"], "Count": [2, 3], "Group_Number": [1, 2]},
         ),  # Test case for multiple sequences belonging to different groups
-    },
+    ),
 }
 
 
 @pytest.mark.parametrize(
-    "sequences, base_sequence, min_count, expected_result",
-    [
-        (
-            data["sequences"],
-            data["base_sequence"],
-            data["min_count"],
-            data["expected_result"],
-        )
-        for data in EXPECTED_RESULTS.values()
-    ],
+    [SEQUENCES, BASE_SEQUENCE, MIN_COUNT, EXPECTED_RESULT],
+    list(EXPECTED_RESULTS.values()),
+    ids=list(EXPECTED_RESULTS.keys()),
 )
 def test_calculate_sequence_variability(
     sequences, base_sequence, min_count, expected_result
