ad16bd54f41953d498ab3509e735cd99069e2dd3
Aniket Fadia <aniketf@uw.edu>
2023-10-10 11:28:40 -0700

    Add form UI to Sequence Haplotypes page (#57)

--- src/app/pages/3_Sequence_Haplotypes.py
+++ src/app/pages/3_Sequence_Haplotypes.py
@@ -2,7 +2,7 @@ from io import BytesIO
 
 import streamlit as st
 
-from app.common.constants import SEQVAR_CALC_BTN, SEQVAR_CALCULATED, SEQVAR_DF
+from app.common.constants import SEQVAR_DF
 from app.common.data_processing import read_fasta
 from app.common.setup import init_session_state_seq_var
 from app.frontend.download_data import download_seq_var_data
@@ -26,35 +26,31 @@ if uploaded_file is not None:
         st.write("Uploaded sequences:")
         st.write(sequences)
 
+with st.form("seq_haplotypes"):
     # Display a form with an input box
     base_sequence = st.text_input("Enter base sequence")
 
-    # Display a button to calculate sequence variability
-    if st.button("Calculate sequence variability"):
-        st.session_state[SEQVAR_CALC_BTN] = True
-
-# Button to calculate sequence variability is clicked
-if st.session_state[SEQVAR_CALC_BTN]:
-    if not base_sequence:
-        st.warning("Please enter a base sequence before calculating.")
-    else:
-        # Calculate sequence variability
-        if not st.session_state[SEQVAR_CALCULATED]:
+    # Button to trigger sequence haplotypes calculation
+    submitted = st.form_submit_button("Find Sequence Haplotypes")
+    if submitted:
+        if not base_sequence:
+            st.warning("Please enter a base sequence before calculating.")
+        else:
+            # Calculate sequence haplotypes
             st.session_state[SEQVAR_DF] = calculate_sequence_variability(
                 sequences, base_sequence
             )
-            # Set the flag for calculation of sequence variability to True
-            st.session_state[SEQVAR_CALCULATED] = True
 
-        # Display the sequence variability data
-        if not st.session_state[SEQVAR_DF].empty:
-            st.write("Sequence Variability Data:")
-            st.dataframe(st.session_state[SEQVAR_DF])
 
-            # Enter the species name for downloading the data
-            species = st.text_input("Enter the species name:")
+# Display the sequence variability data
+if st.session_state[SEQVAR_DF] is not None:
+    st.write("Sequence Variability Data:")
+    st.dataframe(st.session_state[SEQVAR_DF])
 
-            if st.button("Prepare for download"):
-                download_seq_var_data(st.session_state[SEQVAR_DF], species)
-        else:
-            st.write("No sequence variability data to download")
+    # Enter the species name for downloading the data
+    species = st.text_input("Enter the species name:")
+
+    if st.button("Prepare for download"):
+        download_seq_var_data(st.session_state[SEQVAR_DF], species)
+else:
+    st.write("No sequence variability data to download")
