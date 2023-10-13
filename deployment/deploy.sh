bc3937288b331f056b32c89c0d878086a9b477ae
Carlos Garcia Jurado Suarez <carlosg@uw.edu>
2023-10-13 10:28:40 -0700

    Update deployment script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -8,14 +8,19 @@ IMG=$ACR.azurecr.io/neglected-diagnostics
 
 set -e
 # Build a local wheel to help with installing dependencies in the container. See Dockerfile
+rm -rf dist/*.whl
 python -m build --wheel .
 # Can be used to build/run locally:
 # docker build -t ndiag --platform=linux/arm64 -f deployment/Dockerfile .
 # docker run --rm -p 127.0.0.1:8501:80 ndiag
 
 # ACR build is preferrably to docker build since it builds in azure and avoids having to build cross-platform locally
+az account set --subscription "Carlos Garcia Jurado Suarez_SSEC" # Use SSEC subscription
+az group create -l westus3 -n $GRP
+az acr create --name $ACR --resource-group $GRP --sku basic --admin-enabled true
 az acr login --name $ACR
 az acr build --registry $ACR --resource-group $GRP --image $APP -f deployment/Dockerfile .
 az group create -l $LOC -n $GRP
 az acr create --name $ACR --resource-group $GRP --sku basic --admin-enabled true
 az appservice plan create -g $GRP -n $PLAN -l $LOC --is-linux --sku B2
+az webapp create -g $GRP -p $PLAN -n $APP -i $IMG
