ccb9f13682481829fb5d9824d4fbde9f74c6e11c
Carlos Garcia Jurado Suarez <carlosg@uw.edu>
2023-10-19 13:47:08 -0700

    Update path to md file

--- src/app/Home.py
+++ src/app/Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 # Read and display the content from the Markdown file
-with open("home_markdown.md", "r") as f:
+with open("src/app/home_markdown.md", "r") as f:
     content = f.read()
 st.markdown(content)
